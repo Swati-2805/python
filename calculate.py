d9c9686e8c9d612d272ee9e74bd09d6523b8a40b
Swati-2805 <swatitiwari2805@gmail.com>
2025-05-05 01:17:00 +0530

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,26 @@
+
+#WAP to get area of rectangle.
+leng=int(input("Enter the length of the rectangle: "))
+width=int(input("Enter the breadth of the rectangle: "))
+area=leng*width
+print("Enter the arae of the rectangle: ",area)
+----------------------------------------------------------------------------------------------------------------------------------
+#WAp to get area of circle
+radius=int(input("Enter the radius of the circle: "))
+pi=3.14
+area=pi*radius*radius
+print("The area of circle is: ",area)
+-----------------------------------------------------------------------------------------------------------------------------------
+#WAP to get average of three numbers
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+num3=int(input("Enter the third number: "))
+average=(num1+num2+num3)/3
+print("Enter the average of three numbers: ",average)
+-------------------------------------------------------------------------------------------------------------------------------------
+#WAP to check whether The entered number is divisible by 5 or not
+num=int(input("Enter the number: "))
+if(num%5==0):
+    print("The given number is divisible by 5")
+else:
+    print("The given number is not divisible by 5")
